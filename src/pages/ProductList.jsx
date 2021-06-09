e78079b7a5cb1a80338ecfcce64d028d0d21ad48
Kadir Burçin ŞENAY <77517479+kbsenay@users.noreply.github.com>
2021-06-10 00:45:05 +0300

    9 Haziran SON

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { Icon, Label, Menu, Table } from 'semantic-ui-react'
 import ProductService from '../services/productService';
 
@@ -7,8 +8,8 @@ export default function ProductList() {
 
     useEffect(() => {
         let productService = new ProductService()
-        productService.getProducts().then(result=>setProducts(result.data.data))
-    },[])
+        productService.getProducts().then(result => setProducts(result.data.data))
+    }, [])
 
     return (
         <div>
@@ -27,14 +28,13 @@ export default function ProductList() {
                     {
                         products.map(product => (
                             <Table.Row key={product.id}>
-                                <Table.Cell>{product.productName}</Table.Cell>
+                                <Table.Cell><Link to={`/products/${product.productName}`}>{product.productName}</Link></Table.Cell>
                                 <Table.Cell>{product.unitPrice}</Table.Cell>
                                 <Table.Cell>{product.unitsInStock}</Table.Cell>
                                 <Table.Cell>{product.quantityPerUnit}</Table.Cell>
                                 <Table.Cell>{product.category.categoryName}</Table.Cell>
                             </Table.Row>
-                        ))
-                    }
+                        ))}
                 </Table.Body>
 
                 <Table.Footer>
