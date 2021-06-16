bc2c5cbf20e864589276e0e9e53059f874cecad3
Kadir Burçin ŞENAY <77517479+kbsenay@users.noreply.github.com>
2021-06-16 23:36:56 +0300

    16 Haziran 23:36

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import { Icon, Menu, Table } from 'semantic-ui-react'
 import ProductService from '../services/productService';
 
 export default function ProductList() {
